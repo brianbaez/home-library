b41109d79d6d6e387dd998b13e26c916600886ec
Brian Baez <brianbaez.dev@gmail.com>
2022-04-03 18:07:28 -0500

    Updated Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const harryPotterBooks = require("../assets/images/harry-potter-books.jpg");
 function Hero() {
   return (
     <div className="Hero">
-      <div className="container d-flex justify-content-between">
+      <div className="container d-flex">
         <div className="title flex-fill d-flex flex-column justify-content-center">
           <h2 className="fw-regular">Your</h2>
           <h1 className="fw-bolder">HomeLibrary</h1>
@@ -19,7 +19,7 @@ function Hero() {
         </div>
 
         <div className="Books d-none d-md-block ms-5">
-          <img class="img-fluid" src={harryPotterBooks} alt="harry-potter-books"></img>
+          <img className="img-fluid" src={harryPotterBooks} alt="harry-potter-books"></img>
         </div>
       </div>
     </div>
