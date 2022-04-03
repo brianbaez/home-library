cd19b531273f4b3ce5eb979f8a0ac281c34366eb
Brian Baez <brianbaez.dev@gmail.com>
2022-04-03 18:06:59 -0500

    Updated Main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Hero from "../components/Hero";
+import Summary from "../components/Summary";
+import Features from "../components/Features";
+import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
 
 function MainPage() {
   return (
     <div className="Main">
       <Hero />
+      <Summary />
+      <Features />
+      <FAQ />
+      <Footer />
     </div>
   );
 }
