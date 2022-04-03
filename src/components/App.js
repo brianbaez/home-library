72fe33099e352d5924378897bbceca3a2f358fc6
Brian Baez <brianbaez.dev@gmail.com>
2022-04-02 23:09:43 -0500

    Imported MainPage, CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import MainPage from "../pages/MainPage";
+
+import "../assets/styles/App.css";
+
 function App() {
   return (
     <div className="App">
+      <MainPage />
     </div>
   );
 }
