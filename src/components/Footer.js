cbda937de762912b0f5baa656b58d8dbe8ea2909
Brian Baez <brianbaez.dev@gmail.com>
2022-04-03 18:10:01 -0500

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <div className="container text-center pt-4 pb-4">
+        <h3>© HomeLibrary by Brian Baez</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
