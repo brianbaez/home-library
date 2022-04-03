7521020ef7f1800c8ff6ebfd3999b3a98f992224
Brian Baez <brianbaez.dev@gmail.com>
2022-04-02 23:08:00 -0500

    Created Main page

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Hero from "../components/Hero";
+
+function MainPage() {
+  return (
+    <div className="Main">
+      <Hero />
+    </div>
+  );
+}
+
+export default MainPage;
