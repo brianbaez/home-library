4cfdf03d60e6ced4918e3bbbb94f0a8354edba9e
Brian Baez <brianbaez.dev@gmail.com>
2022-04-04 12:57:49 -0500

    Updated FAQ component

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,16 +7,17 @@ function FAQ() {
     <div className="FAQ mt-5 pb-5">
       <div className="container text-center">
         <h2><strong>Frequently Asked Questions</strong></h2>
-        <h3>Didn't find the answer to a question you were looking for? <strong>Contact us!</strong></h3>
 
         {FAQItems.map((item) => {
           return (
             <div key={item.id} className="pt-5">
               <h3><strong>{item.question}</strong></h3>
-              <h3>{item.answer}</h3>
+              <p>{item.answer}</p>
             </div>
           );
         })}
+
+        <h3 className="mt-5">Didn't find the answer to a question you were looking for? <strong>Contact us!</strong></h3>
       </div>
     </div>
   );
