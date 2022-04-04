7b4991ee2e9543cf589c50d21d6b6828a0144a99
Brian Baez <brianbaez.dev@gmail.com>
2022-04-04 14:22:18 -0500

    Updated Summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -10,10 +10,10 @@ function Summary() {
         <h2 className="text-center mb-5">It's never been easier to catalog your <strong>HomeLibrary</strong>, no matter the size of your collection.</h2>
 
         <div className="d-flex flex-column-reverse flex-lg-row">
-          <div className="mt-5 mt-lg-0">
+          <div className="mt-4 mt-lg-0">
             <h2>Why You Should Catalog Your <strong>HomeLibrary</strong></h2>
-            <h3 className="mt-4">Easily track which <strong>books</strong> you own so you don't accidentally buy one you already own!</h3>
-            <h3 className="mt-4">Create <strong>book lists</strong> you can share with family and friends.</h3>
+            <p className="fs-5 mt-4">Easily track which <strong>books</strong> you own so you don't accidentally buy one you already own!</p>
+            <p className="fs-5 mt-4">Create <strong>book lists</strong> you can share with family and friends.</p>
           </div>
 
           <div className="text-center ms-lg-5">
@@ -22,10 +22,10 @@ function Summary() {
         </div>
 
         <div className="d-flex flex-column-reverse flex-lg-row-reverse mt-5">
-          <div className="mt-5 mt-lg-0">
+          <div className="mt-4 mt-lg-0">
             <h2>Keep Track of Your <strong>Reading</strong></h2>
-            <h3 className="mt-4">See how your <strong>reading habits</strong> develop over time by logging your <strong>reading sessions</strong>.</h3>
-            <h3 className="mt-4">We'll show you <strong>charts and graphs</strong> that are fun to see.</h3>
+            <p className="fs-5 mt-4">See how your <strong>reading habits</strong> develop over time by logging your <strong>reading sessions</strong>.</p>
+            <p className="fs-5 mt-4">We'll show you <strong>charts and graphs</strong> that are fun to see.</p>
           </div>
 
           <div className="text-center me-lg-5">
