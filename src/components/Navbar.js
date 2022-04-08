1e5ca991dd3d9c43ee4f95a454e17aa28bca40b1
Brian Baez <brianbaez.dev@gmail.com>
2022-04-08 17:03:37 -0500

    Created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import {navItems} from "./NavbarItems";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav className="container navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand fs-4" href="#">HomeLibrary</a>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarMenu">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {navItems.map((item) => {
+              return (
+                <li key={item.id} className="nav-item ms-lg-1 me-lg-1">
+                  <a className="nav-link" href="#">{item.name}</a>
+                </li>
+              );
+            })}
+          </ul>
+
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search books" aria-label="Search books"></input>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+
+          <div className="Profile mt-3 ms-lg-5 mt-lg-0">
+            <div className="ProfileMenu">
+              <ul className="navbar-nav">
+                <li className="dropdown">
+                  <a className="dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <i class="fa-solid fa-user fa-2x"></i>
+                  </a>
+                  <div className="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+                    <a className="dropdown-item" href="#">Profile</a>
+                    <a className="dropdown-item" href="#">Manage Account</a>
+                    <div class="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Sign Out</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/NavbarItems.js
@@ -0,0 +1,19 @@
+export const navItems = [
+  {
+    id: "1",
+    name: "Home"
+  },
+  {
+    id: "2",
+    name: "My Books"
+  },
+  {
+    id: "3",
+    name: "Stats"
+  },
+  {
+    id: "4",
+    name: "Reading Challenges"
+  },
+
+]
