89184a985628b8350409576e40d5275b58fb6e0c
Brian Baez <brianbaez.dev@gmail.com>
2022-04-11 14:36:48 -0500

    Updated Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,28 @@ function HomePage() {
   return (
     <div className="Home">
       <Navbar />
+
+      <div className="container home-content mt-3 mb-3">
+        <div className="row row-cols-1 row-cols-lg-2">
+          <div className="col mb-3">
+            <h4>Curently Reading</h4>
+          </div>
+
+          <div className="col mb-3">
+            <h4>2022 Reading Challenge</h4>
+          </div>
+
+          <div className="col mb-3">
+            <h4>Read Recently</h4>
+          </div>
+
+          <div className="col mb-3">
+            <h4>Want to Read</h4>
+          </div>
+
+
+        </div>
+      </div>
     </div>
   );
 }
