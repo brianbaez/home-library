efed74a7c39ba59d4094ae4e7d2d3f6f548cf410
Brian Baez <brianbaez.dev@gmail.com>
2022-04-15 17:45:45 -0500

    Implemented signUpHandler

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,7 +1,52 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 function SignUpPage() {
+  let navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const signUpHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    if(password !== confirmPassword) {
+      setPassword("");
+      setConfirmPassword("");
+
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+
+      return setError("Password do not match");
+    }
+
+    try {
+      const {data} = await axios.post("/api/auth/signup", {username, email, password}, config);
+
+      localStorage.setItem("authToken", data.token);
+
+      navigate("/home");
+    }
+    catch(error) {
+      setError(error.response.data.error);
+
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  }
+
   return (
     <div className="SignUp d-flex justify-content-center align-items-center">
       <div className="p-5">
@@ -15,24 +60,26 @@ function SignUpPage() {
         <div className="mt-4">
           <h2 className="text-center">Sign Up</h2>
 
-          <form className="sign-up-form p-4">
+          <form className="sign-up-form p-4" onSubmit={signUpHandler}>
+            {error && <p className="error-message">{error}</p>}
+
             <div className="form-floating mb-3">
-              <input type="email" className="form-control" id="floatingEmail" placeholder="name@example.com"></input>
+              <input required type="email" className="form-control" id="floatingEmail" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)}></input>
               <label htmlFor="floatingEmail">Email address</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input type="text" className="form-control" id="floatingUsername" placeholder="Username"></input>
+              <input required type="text" className="form-control" id="floatingUsername" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
               <label htmlFor="floatingUsername">Username</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+              <input required type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
               <label htmlFor="floatingPassword">Password</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input type="password" className="form-control" id="floatingPasswordConfirmation" placeholder="Confirm Password"></input>
+              <input required type="password" className="form-control" id="floatingPasswordConfirmation" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
               <label htmlFor="floatingPassword">Confirm password</label>
             </div>
 
