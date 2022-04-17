9dd3fdc571945c030d4d45209e653d753b561cf6
Brian Baez <brianbaez.dev@gmail.com>
2022-04-17 03:29:48 -0500

    Updated Private controllers

--- server/controllers/private.js
+++ server/controllers/private.js
@@ -1,36 +1,31 @@
 exports.getPrivateHomeData = (req, res, next) => {
-  console.log("Next!");
   res.status(200).json({
     success: true,
-    data: "You got access to the private home data in this route"
+    message: "You got access to the private home data in this route",
+    data: req.user
   });
-
-  console.log("You got access to the private home data in this route");
 }
 
 exports.getPrivateBooksData = (req, res, next) => {
   res.status(200).json({
     success: true,
-    data: "You got access to the private books data in this route"
+    message: "You got access to the private books data in this route",
+    data: req.user
   });
-
-  console.log("You got access to the private books data in this route");
 }
 
 exports.getPrivateStatsData = (req, res, next) => {
   res.status(200).json({
     success: true,
-    data: "You got access to the private stats data in this route"
+    message: "You got access to the private stats data in this route",
+    data: req.user
   });
-
-  console.log("You got access to the private stats data in this route");
 }
 
 exports.getPrivateChallengesData = (req, res, next) => {
   res.status(200).json({
     success: true,
-    data: "You got access to the private challenges data in this route"
+    message: "You got access to the private challenges data in this route",
+    data: req.user
   });
-
-  console.log("You got access to the private challenges data in this route");
 }
