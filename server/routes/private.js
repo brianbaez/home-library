c7a8385724b438e7425aaefc06c60424786a1a7d
Brian Baez <brianbaez.dev@gmail.com>
2022-04-18 17:34:14 -0500

    Updated private routes

--- server/routes/private.js
+++ server/routes/private.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getPrivateHomeData, getPrivateBooksData, getPrivateStatsData, getPrivateChallengesData} = require("../controllers/private");
 const {protect} = require("../middleware/auth");
+const {getPrivateHomeData, getPrivateBooksData, getPrivateStatsData, getPrivateChallengesData} = require("../controllers/private");
+const {addBook, getBook, deleteBook} = require("../controllers/books");
 
 router.route("/home").get(protect, getPrivateHomeData);
 
@@ -10,5 +11,14 @@ router.route("/my-books").get(protect, getPrivateBooksData);
 router.route("/my-stats").get(protect, getPrivateStatsData);
 
 router.route("/my-reading-challenges").get(protect, getPrivateChallengesData);
+
+// Get book
+router.route("/books/:isbn?").get(protect, getBook);
+
+// Add book
+router.route("/books/").put(protect, addBook);
+
+// Delete book
+router.route("/books/:isbn").delete(protect, deleteBook);
 
 module.exports = router;
