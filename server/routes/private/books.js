2c38b81181b2bfeab1491e057fb4644d4d161006
Brian Baez <brianbaez.dev@gmail.com>
2022-04-23 20:54:04 -0500

    Added a route to edit book

--- server/routes/private/books.js
+++ server/routes/private/books.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {protect} = require("../../middleware/auth");
-const {addBook, getBook, deleteBook} = require("../../controllers/private/books");
+const {addBook, getBook, editBook, deleteBook} = require("../../controllers/private/books");
 
 // Get book(s)
 router.route("/books/:isbn?").get(protect, getBook);
 
 // Add book
 router.route("/books").post(protect, addBook);
+
+// Edit book
+router.route("/books/:isbn").put(protect, editBook);
 
 // Delete book
 router.route("/books/:isbn").delete(protect, deleteBook);
