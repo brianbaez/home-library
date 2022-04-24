58b41653e03db9226482ece5e2187cc12b5b0fd2
Brian Baez <bbaez2@uic.edu>
2022-04-23 20:06:25 -0500

    Updated Books routes

--- server/routes/private/books.js
+++ server/routes/private/books.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const {protect} = require("../../middleware/auth");
 const {addBook, getBook, deleteBook} = require("../../controllers/private/books");
 
-// Get book
+// Get book(s)
 router.route("/books/:isbn?").get(protect, getBook);
 
 // Add book
-router.route("/books").put(protect, addBook);
+router.route("/books").post(protect, addBook);
 
 // Delete book
 router.route("/books/:isbn").delete(protect, deleteBook);
