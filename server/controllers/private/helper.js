af54b1f02d697cf4984992ac47addd56759fee2f
Brian Baez <bbaez2@uic.edu>
2022-04-23 20:03:23 -0500

    Implemented additional helper functions

--- server/controllers/private/helper.js
+++ server/controllers/private/helper.js
@@ -15,6 +15,19 @@ exports.checkBook = async (id, isbn) => {
   );
 }
 
+// Check if review exists
+exports.checkReview = async (id, isbn) => {
+  return await User.aggregate([
+    {$match: {"_id": id}},
+    {$unwind: "$library.books"},
+    {$match: {
+      "library.books.isbn": {$eq: isbn},
+      "library.books.review.rating.wholeNumber": {$ne: -1},
+      "library.books.review.rating.decimalNumber": {$ne: -1}
+    }}
+  ]);
+}
+
 // Check if book is already in the bookshelf
 exports.checkBookshelf = async (id, isbn, bookshelfName) => {
   return await User.aggregate([
@@ -33,5 +46,44 @@ exports.checkChallenge = async (id, year) => {
     {$match: {"_id": id}},
     {$unwind: "$challenges"},
     {$match: {"challenges.year": {$eq: year}}}
+  ]);
+}
+
+// Check if entry exists in journal
+exports.checkJournal = async (id, isbn, entryID) => {
+  return await User.aggregate([
+    {$match: {"_id": id}},
+    {$unwind: "$library.books"},
+    {$match: {"library.books.isbn": {$eq: isbn}}},
+    {$unwind: "$library.books.journal"},
+    {$match: {"library.books.journal._id": entryID}}
+  ]);
+}
+
+// Get all journal entries
+exports.getJournal = async (id, isbn) => {
+  return await User.aggregate([
+    {$match: {"_id": id}},
+    {$unwind: "$library.books"},
+    {$match: {"library.books.isbn": {$eq: isbn}}},
+    {$unwind: "$library.books.journal"},
+    {$group: {
+      "_id": "$_id",
+      "entries": {$push: "$library.books.journal"}
+    }},
+    {$project: {"entries": 1, "_id": 0}}
+  ]);
+}
+
+// Get last journal entry
+exports.getLastEntry = async (id, isbn) => {
+  return await User.aggregate([
+    {$match: {"_id": id}},
+    {$unwind: "$library.books"},
+    {$match: {"library.books.isbn": {$eq: isbn}}},
+    {$project: {
+      "pagesTotal": {$last: "$library.books.journal.pagesReadTotal"}
+    }},
+    {$unwind: "$pagesTotal"}
   ]);
 }
