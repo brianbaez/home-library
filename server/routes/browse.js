5170e07d7bd88029000ac6c9056734d2792fd481
Brian Baez <brianbaez.dev@gmail.com>
2022-04-24 16:08:39 -0500

    Crated a Browse books route

---
+++ server/routes/browse.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const {browseBooks} = require("../controllers/browse");
+
+// Get book results  from Google Books API
+router.route("/browse").get(browseBooks);
+
+module.exports = router;
