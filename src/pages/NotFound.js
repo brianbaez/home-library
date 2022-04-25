ca530a00cc5fad1711c2be01c3a5be6026a8ed4e
Brian Baez <brianbaez.dev@gmail.com>
2022-04-25 17:12:29 -0500

    Created a NotFound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import useAuth from "../components/hooks/useAuth";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>This page was not found...</h1>
+    </div>
+  );
+}
+
+export default NotFound;
