97ed6e14634c698eb234a18513b18a656f65229b
Brian Baez <bbaez2@uic.edu>
2022-04-25 01:51:17 -0500

    Deleted getBookCovers function

--- server/controllers/browse.js
+++ server/controllers/browse.js
@@ -32,45 +32,20 @@ const getBookResults = async (selfLinks, next) => {
   await Promise.all(selfLinks.map((url) => axios.get(url)))
   .then((res) => {
     res.forEach((item) => {
-      results.push({
-        title: item.data.volumeInfo.title,
-        authors: item.data.volumeInfo.authors,
-        description: item.data.volumeInfo.description,
-        isbn: item.data.volumeInfo.industryIdentifiers,
-        pages: item.data.volumeInfo.pageCount,
-        cover: item.data.volumeInfo.imageLinks
-      });
-    });
-  })
-  .catch((error) => {
-    return next(new ErrorResponse("Failed to get book results", 500));
-  });
-
-  return results;
-}
-
-const getBookCovers = async (results, next) => {
-  // Set endpoints
-  let endpoints = [];
-
-  results.forEach(item => {
-    endpoints.push("https://bookcoverapi.herokuapp.com/getBookCover?bookTitle=" + item.title + "&authorName=" + item.authors[0]);
-  });
-
-  // Get all book covers
-  // Using: https://github.com/w3slley/bookcover-api
-  await Promise.all(endpoints.map((url) => axios.get(url)))
-  .then((res) => {
-    res.forEach((item, index) => {
-      if(item.data.bookCoverUrl !== undefined) {
-        if(item.data.bookCoverUrl.startsWith("https://i.gr-assets.com/images/")) {
-          results[index].cover = item.data.bookCoverUrl;
-        }
+      if(item.data.volumeInfo.title !== undefined && item.data.volumeInfo.description !== undefined) {
+        results.push({
+          title: item.data.volumeInfo.title,
+          authors: item.data.volumeInfo.authors,
+          description: item.data.volumeInfo.description,
+          isbn: item.data.volumeInfo.industryIdentifiers,
+          pages: item.data.volumeInfo.pageCount,
+          cover: item.data.volumeInfo.imageLinks
+        });
       }
     });
   })
   .catch((error) => {
-    return next(new ErrorResponse("Failed to get book covers", 500));
+    return next(new ErrorResponse("Failed to get book results", 500));
   });
 
   return results;
