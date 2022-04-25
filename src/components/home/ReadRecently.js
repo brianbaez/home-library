ec6eeef0f5946a28ab65517d316d038b96c438ff
Brian Baez <brianbaez.dev@gmail.com>
2022-04-25 17:13:56 -0500

    Created a ReadRecently component to fetch user data

---
+++ src/components/home/ReadRecently.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+function ReadRecently({config}) {
+  const [readRecently, setReadRecently] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    console.log("Rendering ReadRecently...");
+
+    const fetchReadRecently = async () => {
+      await axios.get(`/api/private/bookshelves/read`, config)
+      .then((res) => {
+        setReadRecently(res.data.data)
+      })
+      .catch((error) => {
+        setError(error.response.data.error);
+      });
+    }
+
+    fetchReadRecently();
+  }, []);
+
+  return (
+    <div className="col mb-3">
+      <h4>Read Recently</h4>
+      <pre>{JSON.stringify(readRecently, null, 2)}</pre>
+      {error && <span className="error-message">{error}</span>}
+    </div>
+  );
+}
+
+export default ReadRecently;
