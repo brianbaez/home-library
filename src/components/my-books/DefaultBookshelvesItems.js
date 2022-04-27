79999d54ce2dba47bf90dc7b06497c471419065f
Brian Baez <brianbaez.dev@gmail.com>
2022-04-27 16:45:55 -0500

    Created array of default bookshelves

---
+++ src/components/my-books/DefaultBookshelvesItems.js
@@ -0,0 +1,42 @@
+export const defaultBookshelvesItems = [
+  {
+    id: "0",
+    name: "All",
+    tag: ""
+  },
+  {
+    id: "1",
+    name: "Read",
+    tag: "read"
+  },
+  {
+    id: "2",
+    name: "Currently Reading",
+    tag: "currently-reading"
+  },
+  {
+    id: "3",
+    name: "Want to Read",
+    tag: "want-to-read"
+  },
+  {
+    id: "4",
+    name: "Did Not Finish",
+    tag: "did-not-finish"
+  },
+  {
+    id: "5",
+    name: "Wish List",
+    tag: "wish-list"
+  },
+  {
+    id: "6",
+    name: "In My Library",
+    tag: "in-my-library"
+  },
+  {
+    id: "7",
+    name: "Reviewed",
+    tag: "reviewed"
+  }
+]
