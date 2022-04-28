2bc1eb58bb8130bc4dc32f4b282b9067c518b29c
Brian Baez <brianbaez.dev@gmail.com>
2022-04-28 03:46:25 -0500

    Updated Browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -2,34 +2,18 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {useSearchParams, useLocation} from "react-router-dom";
 
-function Browse() {
-  const location = useLocation();
-  const [error, setError] = useState();
+// Components
+import Results from "../components/browse/Results";
+
+function Browse(defaultBookshelves) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get("search");
+  const query = searchParams.get("search");
 
-  const results = location.state.results;
-
-  if(!search) {
-    return (
-      <div className="Browse">
-        <div className="container home-content mt-3 mb-3">
-          <h1>No results</h1>
-        </div>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div className="Browse">
-        <div className="container home-content mt-3 mb-3">
-          <h1>Search results for {search}</h1>
-          {error && <span className="error-message">{error}</span>}
-          <pre>{JSON.stringify(results, null, 2)}</pre>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div className="Browse">
+      <Results query={query}/>
+    </div>
+  );
 }
 
 export default Browse;
