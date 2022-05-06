caecefbba744a8fa70cf7c2d57226d176b5f3b38
Brian Baez <brianbaez.dev@gmail.com>
2022-05-06 15:48:15 -0500

    Added a Route/Controller to get, update, and delete a user account

---
+++ server/controllers/private/account.js
@@ -0,0 +1,129 @@
+const User = require("../../models/User");
+const ErrorResponse = require("../../utils/ErrorResponse");
+const {checkUser} = require("./helper");
+
+exports.getUserData = async (req, res, next) => {
+  const userData = req.user;
+
+  try {
+    if(!(await checkUser(userData._id))) {
+      return next(new ErrorResponse("Failed to get user data", 401));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User data sent",
+      data: {
+        id: userData._id,
+        email: userData.email,
+        username: userData.username
+      }
+    });
+  }
+  catch {
+    return next(new ErrorResponse("Failed to get user data", 401));
+  }
+}
+
+exports.updateEmail = async (req, res, next) => {
+  const userData = req.user;
+  const {newEmail} = req.body;
+
+  try {
+    if(!(await checkUser(userData._id))) {
+      return next(new ErrorResponse("Failed to update user email", 401));
+    }
+
+    if(newEmail === userData.email) {
+      return next(new ErrorResponse("Please provide a different email"));
+    }
+
+    await User.updateOne(
+      {"_id": userData._id},
+      {$set: {"email": newEmail}}
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Email has been updated"
+    });
+  }
+  catch {
+    return next(new ErrorResponse("Failed to update user email", 401));
+  }
+}
+
+exports.updateUsername = async (req, res, next) => {
+  const userData = req.user;
+  const {newUsername} = req.body;
+
+  try {
+    if(!(await checkUser(userData._id))) {
+      return next(new ErrorResponse("Failed to update username", 401));
+    }
+
+    if(newUsername === userData.username) {
+      return next(new ErrorResponse("Please provide a different username"));
+    }
+
+    await User.updateOne(
+      {"_id": userData._id},
+      {$set: {"username": newUsername}}
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Username has been updated"
+    });
+  }
+  catch {
+    return next(new ErrorResponse("Failed to update username", 401));
+  }
+}
+
+exports.updatePassword = async (req, res, next) => {
+  const userData = req.user;
+  const {newPassword} = req.body;
+
+  try {
+    const user = await checkUser(userData._id);
+
+    if(!user) {
+      return next(new ErrorResponse("Failed to update password", 401));
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      successs: true,
+      message: "Password reset successfully"
+    })
+  }
+  catch {
+    return next(new ErrorResponse("Failed to update password", 401));
+  }
+}
+
+exports.deleteAccount = async (req, res, next) => {
+  const userData = req.user;
+
+  try {
+    if(!(await checkUser(userData._id))) {
+      return next(new ErrorResponse("Failed to delete account", 401));
+    }
+
+    await User.findOneAndRemove(
+      {"_id": userData._id}
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Account has been deleted"
+    });
+  }
+  catch {
+    return next(new ErrorResponse("Failed to delete account", 401));
+  }
+}
---
+++ server/routes/private/account.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {protect} = require("../../middleware/auth");
+const {getUserData, updateEmail, updateUsername, updatePassword, deleteAccount} = require("../../controllers/private/account");
+
+// Get user's email and username
+router.route("/account").get(protect, getUserData);
+
+// Update user email
+router.route("/account/edit/email").put(protect, updateEmail);
+
+// Update username
+router.route("/account/edit/username").put(protect, updateUsername);
+
+// Update user password
+router.route("/account/edit/password").put(protect, updatePassword);
+
+// Delete book
+router.route("/account/delete").delete(protect, deleteAccount);
+
+module.exports = router;
