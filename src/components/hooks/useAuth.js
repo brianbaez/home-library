d750af9c02de24bc5efc441c4e8caf1f4bef345d
Brian Baez <brianbaez.dev@gmail.com>
2022-05-07 15:23:06 -0500

    Updated useAuth hook

--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -1,27 +1,17 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const useAuth = (props) => {
+function useAuth({config}) {
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     const authHandler = async (e) => {
-
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${localStorage.getItem("authToken")}`
-        }
-      };
-
       try {
-        const {data} = await axios.get(`/api/private/${props.path}`, config);
-
+        const {data} = await axios.get(`/api/private/user`, config);
         setIsAuth(true);
       }
       catch(error) {
         localStorage.removeItem("authToken");
-
         setIsAuth(false);
       }
     };
