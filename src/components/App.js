8d4a51b8fec6bb4e74630df3456e81b2616d8f56
Brian Baez <brianbaez.dev@gmail.com>
2022-05-07 20:45:21 -0500

    Updated App component

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,11 @@ import ResetPasswordPage from "../pages/ResetPasswordPage";
 import HomePage from "../pages/HomePage";
 import MyBooksPage from "../pages/MyBooksPage";
 import BookPage from "../pages/BookPage";
-import StatsPage from "../pages/StatsPage";
 import ReadingChallengesPage from "../pages/ReadingChallengesPage";
-import Browse from "../pages/Browse";
+import BrowsePage from "../pages/BrowsePage";
+import JournalPage from "../pages/JournalPage";
+import EntryPage from "../pages/EntryPage";
+import ManageAccountPage from "../pages/ManageAccountPage";
 import NotFound from "../pages/NotFound";
 
 // Routing
@@ -22,22 +24,33 @@ import PrivateRoute from "./routing/PrivateRoute";
 import "../assets/styles/App.css";
 
 function App() {
+  const config = {
+    header: {
+      "Content-Type": "application/json"
+    }
+  };
+
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route exact path="/" element={<MainPage />} />
-          <Route exact path="/signin" element={<SignInPage />} />
-          <Route exact path="/signup" element={<SignUpPage />} />
-          <Route exact path="/forgotpassword" element={<ForgotPasswordPage />} />
-          <Route exact path="/resetpassword/:resetToken" element={<ResetPasswordPage />} />
+          <Route exact path="/signin" element={<SignInPage config={config}/>} />
+          <Route exact path="/signup" element={<SignUpPage config={config}/>} />
+          <Route exact path="/forgotpassword" element={<ForgotPasswordPage config={config}/>} />
+          <Route exact path="/resetpassword/:resetToken" element={<ResetPasswordPage config={config}/>} />
 
           <Route element={<PrivateRoute />}>
             <Route exact path="/home" element={<HomePage />} />
-            <Route exact path="/my-books" element={<MyBooksPage />} />
-            <Route exact path="/my-stats" element={<StatsPage />} />
+            <Route exact path="/my-books/" element={<MyBooksPage />} />
+            <Route exact path="/my-books/:bookshelfParam" element={<MyBooksPage />} />
             <Route exact path="/my-reading-challenges" element={<ReadingChallengesPage />} />
-            <Route exact path="/browse" element={<Browse />} />
+            <Route exact path="/browse" element={<BrowsePage />} />
+            <Route exact path="/book/:isbn" element={<BookPage />} />
+            <Route exact path="/journal/:isbn" element={<JournalPage />} />
+            <Route exact path="/journal/:isbn/edit/:entryID" element={<EntryPage />} />
+            <Route exact path="/journal/:isbn/add" element={<EntryPage />} />
+            <Route exact path="/account" element={<ManageAccountPage />} />
             <Route exact path="*" element={<NotFound />} />
           </Route>
         </Routes>
