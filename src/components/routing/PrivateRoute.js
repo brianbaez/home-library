98e12fb44052a0753a0b91f17800d3187ce5187d
Brian Baez <brianbaez.dev@gmail.com>
2022-05-07 20:41:51 -0500

    Updated PrivateRoute

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import {Navigate, Outlet} from 'react-router-dom';
 
+// Components
 import Navbar from "../Navbar";
 
 function PrivateRoute() {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem("authToken")}`
+    }
+  };
+
   return localStorage.getItem("authToken")
   ?
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={config}/>
     </>
   : <Navigate to="/signin" />
 }
