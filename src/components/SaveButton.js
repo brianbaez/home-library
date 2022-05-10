d5960d2ba9d4b59d55e5970b055f78be4f06dfc5
Brian Baez <brianbaez.dev@gmail.com>
2022-05-10 00:14:58 -0500

    Updated SaveButton component

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function SaveButton({success, error}) {
   return (
-    <div className="SaveButton mt-3">
+    <div className="SaveButton">
       <button className="btn" type="submit">Save</button>
       {success && <span className="ms-3">{success}</span>}
       {error && <span className="ms-3">{error}</span>}
