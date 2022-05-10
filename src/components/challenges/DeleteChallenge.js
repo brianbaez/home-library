b65167f37c5c011cd273f8dd7fd112c3c9908e35
Brian Baez <brianbaez.dev@gmail.com>
2022-05-10 00:12:19 -0500

    Created DeleteChallenge component

---
+++ src/components/challenges/DeleteChallenge.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+
+function DeleteChallenge(deleteChallengeProps) {
+  // Props
+  const {config, year, success, setSuccess} = deleteChallengeProps;
+
+  const deleteChallengeHandler = async (e) => {
+    e.preventDefault();
+
+    if(window.confirm("Are you sure you want to delete this challenge? All records will be deleted.")) {
+      await axios.delete(`/api/private/challenges/${year}`, config)
+      .then((res) => {
+        setSuccess(res.data.message);
+
+        setTimeout(() => {
+          setSuccess();
+        }, 1000);
+      })
+      .catch((error) => {})
+    }
+  }
+  return (
+    <div className="DeleteChallenge mt-3">
+      <a href="#" onClick={deleteChallengeHandler} style={{color: "red"}}>Delete {year} challenge</a>
+    </div>
+  );
+}
+
+export default DeleteChallenge;
