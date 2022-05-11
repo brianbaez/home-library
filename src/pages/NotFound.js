3aa6d74aba7cf3c2b6c52f729462918ea78ec92f
Brian Baez <bbaez2@uic.edu>
2022-05-11 13:47:34 -0500

    Updated NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,8 +3,12 @@ import useAuth from "../components/hooks/useAuth";
 
 function NotFound() {
   return (
-    <div>
-      <h1>This page was not found...</h1>
+    <div className="NotFound d-flex justify-content-center align-items-center">
+      <div className="text-center">
+        <h1>Oops!</h1>
+        <h2>The page you are looking for was not found.</h2>
+        <a className="btn mt-3" href="/">Back to the HomeLibrary homepage</a>
+      </div>
     </div>
   );
 }
