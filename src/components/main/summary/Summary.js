b74fd8c61ea07c9dfb6423e5e46b353e4e262f32
Brian Baez <bbaez2@uic.edu>
2022-05-12 12:06:15 -0500

    Updated

--- src/components/main/summary/Summary.js
+++ src/components/main/summary/Summary.js
@@ -8,7 +8,7 @@ function Summary() {
   return (
     <div className="Summary">
       <div className="container mt-5">
-        <h2 className="text-center mb-5">It's never been easier to catalog your <strong>HomeLibrary</strong>, no matter the size of your collection.</h2>
+        <h2 className="text-center mb-5">It's never been easier to catalog your <strong>HomeLibrary</strong></h2>
 
         <div className="d-flex flex-column-reverse flex-lg-row">
           <div className="mt-4 mt-lg-0">
