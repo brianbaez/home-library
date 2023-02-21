3bf4f98c24003611dc2a6ec3f45fca1df693907e
Brian Baez <brianbaez.dev@gmail.com>
2023-02-20 20:09:01 -0600

    Updated Axios baseURL for Netlify domain

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://stunning-gecko-4a513a.netlify.app"
 });
 
 export default axiosInstance;
