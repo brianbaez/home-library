d19c79a969e49642d767dce818ac8a554842d05d
Brian Baez <brianbaez.dev@gmail.com>
2023-02-20 20:12:42 -0600

    Updated Axios baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://stunning-gecko-4a513a.netlify.app"
+    baseURL: "http://localhost:5000"
 });
 
 export default axiosInstance;
